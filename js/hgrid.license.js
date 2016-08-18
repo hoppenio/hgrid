5403b38b4bb49ed8700b8012863334e1ea6a5613
hoppenio <hoppen@u-core.co.kr>
2016-08-18 14:33:31 +0900

    라이센스수정, 데모스타일 추가

--- js/hgrid.license.js
+++ js/hgrid.license.js
@@ -2,7 +2,7 @@
  * HGrid 1.0x License
  * License Type : Enterprise
  * Client ID : JSFFIDDLE
- * Host Domain : jsfiddle.net,www.jsfiddle.net
+ * Host Domain : jsfiddle.net,www.jsfiddle.net,www.jsfiddle.net
  * Expiration Date : 2020/12/31
  */
-var HOPPENLicense = "AKcRTxNePhMCVTfQg/BBZMkRmA43+OWU83y/Bp2TD3IiMkzghAlFTw6uf7a0z5OtDxU+zX2O5dfdt9xivi6cvL2fMdGH0HFh2diVjg0ueqs8vUFKsu9HPd4PjQxVnt+w";
+var HOPPENLicense = "AKcRTxNePhMCVTfQg/BBZMkRmA43+OWU83y/Bp2TD3IiMkzghAlFTw6uf7a0z5OtDxU+zX2O5dfdt9xivi6cvL3eUgg4X7FK/i6/xSpN/UWkF/lr2kEu276WZHieto0JKOI98apKLJ/SOpIV3QMoQA==";
