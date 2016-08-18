035b8f6dfe642c51797b6b63f5feb0fa127a3d28
hoppenio <hoppen@u-core.co.kr>
2016-08-18 14:04:16 +0900

    라이센스변경

--- js/hgrid.license.js
+++ js/hgrid.license.js
@@ -1,8 +1,8 @@
 /**
  * HGrid 1.0x License
  * License Type : Enterprise
- * Client ID : CODEPEN
- * Host Domain : codepen.io,www.codepen.io
+ * Client ID : JSFFIDDLE
+ * Host Domain : jsfiddle.net,www.jsfiddle.net
  * Expiration Date : 2020/12/31
  */
-var HOPPENLicense = "AKcRTxNePhMCVTfQg/BBZBN4GmEFlPDblPiC38cqQaEoJ/gg2ytZGkW11e29stsOo0rdMM+skCkuKwQ+sEaA8p8ca6q8SGdiOI56pcu0kVmsp3VRn6U2muJggwOZcvCD";
+var HOPPENLicense = "AKcRTxNePhMCVTfQg/BBZMkRmA43+OWU83y/Bp2TD3IiMkzghAlFTw6uf7a0z5OtDxU+zX2O5dfdt9xivi6cvL2fMdGH0HFh2diVjg0ueqs8vUFKsu9HPd4PjQxVnt+w";
